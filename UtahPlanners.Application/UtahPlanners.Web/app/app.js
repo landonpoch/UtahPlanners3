d2d3a92f788ddfd61e53d2be1314e07011334139
Landon Poch <landon.poch+git@gmail.com>
2014-08-20 12:19:01 -0500

    Starting to build out the angular.js logic for authentication.

---
+++ UtahPlanners.Application/UtahPlanners.Web/app/app.js
@@ -0,0 +1,24 @@
+﻿var app = angular.module('UtahPlannersApp', ['ngRoute', 'LocalStorageModule', 'angular-loading-bar']);
+
+app.config(function ($routeProvider) {
+    $routeProvider.when("/home", {
+        controller: "homeController",
+        templateUrl: "/app/views/home.html"
+    });
+
+    $routeProvider.when("/login", {
+        controller: "loginController",
+        templateUrl: "/app/views/login.html"
+    });
+
+    $routeProvider.when("/signup", {
+        controller: "signupController",
+        templateUrl: "/app/views/signup.html"
+    });
+
+    $routeProvider.otherwise({ redirectTo: "/home" });
+});
+
+app.run(['authService', function (authService) {
+    authService.fillAuthData();
+}]);
